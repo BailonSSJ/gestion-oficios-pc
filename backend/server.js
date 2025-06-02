60a3cfae32e2903e62b8b2ce83e2e5b283623a15
BailonSSJ <bailonssj@gmail.com>
2025-06-02 13:32:13 -0600

    update link frontend de netlify en backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://oficialidad-departes-pcivilaca.netlify.app'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
